0db1f42d4a9bb73b48e909921aa0259ac7511ba2
MetaFomos <dev.metafomos@gmail.com>
2023-05-24 18:43:55 +0900

    finish home page

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const HomePage = () => {
+  const [content, setContent] = useState("");
+
+  const init = async () => {
+    console.log("document loaded");
+    const res = await axios.get("https://oxen.observer/");
+    let response = res.data;
+    response = response.split("Oxen").join("Selam").split("OXEN").join("SELAM");
+
+    console.log(response.indexOf("Server Time"));
+    const remove1 = response.split("Server Time");
+    const remove2 = remove1[1].split("minus any portion that was burned.");
+
+    let new1 = [remove1[0], remove2[1]].join();
+    setContent(new1);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return (
+    <>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </>
+  );
+};
+
+export default HomePage;
